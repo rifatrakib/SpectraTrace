50422d3468891c645a96da4c2a998b14b3d9e0af
rifatrakib <Rakib.1508@outlook.com>
2023-06-08 19:11:30 +0600

    create new module to produce app events

---
+++ server/events/startup.py
@@ -0,0 +1,138 @@
+from typing import Any, Dict
+
+from pydantic import parse_file_as
+
+from server.config.factory import settings
+from server.models.users import UserAccount
+from server.schemas.inc.audit import AuditRequestSchema, MetadataSchema
+
+
+def db_initialization_event(execution_time: float, sql: str) -> AuditRequestSchema:
+    event_data: AuditRequestSchema = parse_file_as(
+        type_=AuditRequestSchema,
+        path="server/templates/events/rdbms-events.json",
+    )
+
+    event_data.status = "success"
+    event_data.level = "info"
+    event_data.event.name = "app startup"
+    event_data.event.type = "initial configuration"
+    event_data.event.total_duration = execution_time
+    event_data.event.affected_resources = 1
+    event_data.event.latency = execution_time / 3 * 2
+    event_data.event.cpu_usage = 0.3  # sample value
+    event_data.event.memory_usage = 0.1  # sample value
+    event_data.event.detail["sql"] = sql
+    event_data.event.description = "Create necessary tables in PostgreSQL database"
+    event_data.actor.detail["location"] = "server/main.py - line 146"
+    event_data.resource.detail["database"] = settings.POSTGRES_DB
+    event_data.resource.detail["tables"] = ["accounts"]
+
+    return event_data
+
+
+def influxdb_onboarding_event(
+    execution_time: float,
+    url: str,
+    payload: Dict[str, str],
+    headers: Dict[str, str],
+    response: Dict[str, Any],
+) -> AuditRequestSchema:
+    event_data: AuditRequestSchema = parse_file_as(
+        type_=AuditRequestSchema,
+        path="server/templates/events/influx-events.json",
+    )
+
+    event_data.status = "success"
+    event_data.level = "info"
+    event_data.event.name = "app startup"
+    event_data.event.type = "initial configuration"
+    event_data.event.total_duration = execution_time
+    event_data.event.affected_resources = 1
+    event_data.event.latency = execution_time / 3 * 2
+    event_data.event.cpu_usage = 0.05  # sample value
+    event_data.event.memory_usage = 0.15  # sample value
+    event_data.event.detail["url"] = url
+    event_data.event.detail["method"] = "POST"
+    event_data.event.detail["payload"] = payload
+    event_data.event.detail["headers"] = headers
+    event_data.event.description = "Create admin account in InfluxDB database via HTTP endpoint"
+    event_data.actor.detail["location"] = "server/main.py::on_startup - line 46"
+    event_data.resource.detail["bucket"] = settings.INFLUXDB_USER
+    event_data.resource.detail["organization"] = settings.INFLUXDB_ORG
+    event_data.resource.detail["api_token"] = response["auth"]["token"]
+    event_data.resource.detail["permissions"] = response["auth"]["permissions"]
+
+    return event_data
+
+
+def admin_account_create_event(
+    execution_time: float,
+    sql: str,
+    user: UserAccount,
+) -> AuditRequestSchema:
+    event_data: AuditRequestSchema = parse_file_as(
+        type_=AuditRequestSchema,
+        path="server/templates/events/rdbms-events.json",
+    )
+
+    event_data.status = "success"
+    event_data.level = "info"
+    event_data.event.name = "app startup"
+    event_data.event.type = "initial configuration"
+    event_data.event.total_duration = execution_time
+    event_data.event.affected_resources = 1
+    event_data.event.latency = execution_time / 3 * 2
+    event_data.event.cpu_usage = 0.25  # sample value
+    event_data.event.memory_usage = 0.2  # sample value
+    event_data.event.detail["sql"] = sql
+    event_data.event.description = "Create admin account in PostgreSQL database"
+    event_data.actor.detail["location"] = "server/main.py::on_startup - line 80"
+    event_data.resource.detail["database"] = settings.POSTGRES_DB
+    event_data.resource.detail["tables"] = ["accounts"]
+    event_data.metadata = [
+        MetadataSchema(
+            is_metric=False,
+            name="admin_account",
+            value=user.json(),
+        ),
+    ]
+
+    return event_data
+
+
+def check_admin_account_event(
+    execution_time: float,
+    sql: str,
+    user: UserAccount,
+) -> AuditRequestSchema:
+    event_data: AuditRequestSchema = parse_file_as(
+        type_=AuditRequestSchema,
+        path="server/templates/events/rdbms-events.json",
+    )
+
+    event_data.status = "success"
+    event_data.level = "info"
+    event_data.event.name = "app startup"
+    event_data.event.type = "initial configuration"
+    event_data.event.total_duration = execution_time
+    event_data.event.affected_resources = 1
+    event_data.event.latency = execution_time / 3 * 2
+    event_data.event.cpu_usage = 0.1  # sample value
+    event_data.event.memory_usage = 0.1  # sample value
+    event_data.event.detail["sql"] = sql
+    event_data.event.description = "Check if admin account exists in PostgreSQL database"
+    event_data.actor.detail["location"] = "server/main.py::on_startup - line 101"
+    event_data.resource.detail["database"] = settings.POSTGRES_DB
+    event_data.resource.detail["tables"] = ["accounts"]
+
+    if user:
+        event_data.metadata = [
+            MetadataSchema(
+                is_metric=False,
+                name="admin_account",
+                value=user.json(),
+            ),
+        ]
+
+    return event_data
--- server/main.py
+++ server/main.py
@@ -4,18 +4,22 @@ from time import time
 
 import requests
 from fastapi import FastAPI
-from pydantic import parse_file_as
 from sqlmodel import Session, create_engine, select
 
 from server.config.factory import settings
 from server.database.managers import create_db_and_tables, ping_redis_server
 from server.docs.manager import read_api_metadata, read_tags_metadata
+from server.events.startup import (
+    admin_account_create_event,
+    check_admin_account_event,
+    db_initialization_event,
+    influxdb_onboarding_event,
+)
 from server.models.users import UserAccount
 from server.routes.audit import router as audit_router
 from server.routes.auth import router as auth_router
 from server.routes.user import router as user_router
 from server.schemas.base import HealthResponseSchema
-from server.schemas.inc.audit import AuditRequestSchema, MetadataSchema
 from server.security.auth.authentication import pwd_context
 from server.utils.enums import Tags
 from server.utils.generators import generate_random_key
@@ -28,11 +32,6 @@ app = FastAPI(
 
 
 def influxdb_onboarding(base_url, user, password, organization):
-    event_data: AuditRequestSchema = parse_file_as(
-        type_=AuditRequestSchema,
-        path="server/templates/events/influx-events.json",
-    )
-
     payload = {"username": user, "password": password, "org": organization, "bucket": user}
     headers = {
         "sec-ch-ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
@@ -51,38 +50,17 @@ def influxdb_onboarding(base_url, user, password, organization):
     execution_time = (time() - start_time) * 1000
     response = r.json()
 
-    event_data.status = "success"
-    event_data.level = "info"
-    event_data.event.name = "app startup"
-    event_data.event.type = "initial configuration"
-    event_data.event.total_duration = execution_time
-    event_data.event.affected_resources = 1
-    event_data.event.latency = execution_time // 3
-    event_data.event.cpu_usage = 0.05  # sample value
-    event_data.event.memory_usage = 0.15  # sample value
-    event_data.event.detail["url"] = f"{base_url}/api/v2/setup"
-    event_data.event.detail["method"] = "POST"
-    event_data.event.detail["payload"] = payload
-    event_data.event.detail["headers"] = headers
-    event_data.event.description = "Create admin account in InfluxDB database via HTTP endpoint"
-    event_data.actor.detail["location"] = [
-        "server/main.py::on_startup - line 100",
-        "server/main.py::admin_account_exists - line 100",
-    ]
-    event_data.resource.detail["bucket"] = settings.INFLUXDB_USER
-    event_data.resource.detail["organization"] = settings.INFLUXDB_ORG
-    event_data.resource.detail["api_token"] = response["auth"]["token"]
-    event_data.resource.detail["permissions"] = response["auth"]["permissions"]
-
+    event_data = influxdb_onboarding_event(
+        execution_time=execution_time,
+        url=f"{base_url}/api/v2/setup",
+        payload=payload,
+        headers=headers,
+        response=response,
+    )
     return event_data
 
 
 def create_admin_account(username, password, organization, api_token):
-    event_data: AuditRequestSchema = parse_file_as(
-        type_=AuditRequestSchema,
-        path="server/templates/events/rdbms-events.json",
-    )
-
     engine = create_engine(settings.RDS_URI, echo=True)
     log_stream = io.StringIO()
     handler = logging.StreamHandler(log_stream)
@@ -109,40 +87,15 @@ def create_admin_account(username, password, organization, api_token):
     sql = log_stream.getvalue()
     log_stream.close()
 
-    event_data.status = "success"
-    event_data.level = "info"
-    event_data.event.name = "app startup"
-    event_data.event.type = "initial configuration"
-    event_data.event.total_duration = execution_time
-    event_data.event.affected_resources = 1
-    event_data.event.latency = execution_time // 3
-    event_data.event.cpu_usage = 0.25  # sample value
-    event_data.event.memory_usage = 0.2  # sample value
-    event_data.event.detail["sql"] = sql
-    event_data.event.description = "Create admin account in PostgreSQL database"
-    event_data.actor.detail["location"] = [
-        "server/main.py::on_startup - line 100",
-        "server/main.py::admin_account_exists - line 100",
-    ]
-    event_data.resource.detail["database"] = settings.POSTGRES_DB
-    event_data.resource.detail["tables"] = ["accounts"]
-    event_data.metadata = [
-        MetadataSchema(
-            is_metric=False,
-            name="admin_account",
-            value=user.json(),
-        ),
-    ]
-
+    event_data = admin_account_create_event(
+        execution_time=execution_time,
+        sql=sql,
+        user=user,
+    )
     return event_data, user
 
 
 def admin_account_exists(username: str) -> bool:
-    event_data: AuditRequestSchema = parse_file_as(
-        type_=AuditRequestSchema,
-        path="server/templates/events/rdbms-events.json",
-    )
-
     engine = create_engine(settings.RDS_URI, echo=True)
     session = Session(engine)
 
@@ -152,35 +105,12 @@ def admin_account_exists(username: str) -> bool:
     execution_time = (time() - start_time) * 1000
     sql = str(stmt.compile(engine, compile_kwargs={"literal_binds": True}))
 
-    event_data.status = "success"
-    event_data.level = "info"
-    event_data.event.name = "app startup"
-    event_data.event.type = "initial configuration"
-    event_data.event.total_duration = execution_time
-    event_data.event.affected_resources = 1
-    event_data.event.latency = execution_time // 3
-    event_data.event.cpu_usage = 0.1  # sample value
-    event_data.event.memory_usage = 0.1  # sample value
-    event_data.event.detail["sql"] = sql
-    event_data.event.description = "Check if admin account exists in PostgreSQL database"
-    event_data.actor.detail["location"] = [
-        "server/main.py::on_startup - line 100",
-        "server/main.py::admin_account_exists - line 100",
-    ]
-    event_data.resource.detail["database"] = settings.POSTGRES_DB
-    event_data.resource.detail["tables"] = ["accounts"]
-
-    if user:
-        event_data.metadata = [
-            MetadataSchema(
-                is_metric=False,
-                name="admin_account",
-                value=user.json(),
-            ),
-        ]
-        return event_data, user
-
-    return event_data, None
+    event_data = check_admin_account_event(
+        execution_time=execution_time,
+        sql=sql,
+        user=user,
+    )
+    return event_data, user
 
 
 def create_admin_credentials():
@@ -213,34 +143,13 @@ def on_startup():
     startup_events = []
 
     print("Starting up...")
-    event_data: AuditRequestSchema = parse_file_as(
-        type_=AuditRequestSchema,
-        path="server/templates/events/rdbms-events.json",
-    )
 
     print("Creating relational database and tables...")
     start_time = time()
     sql = create_db_and_tables()
     execution_time = (time() - start_time) * 1000
 
-    event_data.status = "success"
-    event_data.level = "info"
-    event_data.event.name = "app startup"
-    event_data.event.type = "initial configuration"
-    event_data.event.total_duration = execution_time
-    event_data.event.affected_resources = 1
-    event_data.event.latency = execution_time // 3
-    event_data.event.cpu_usage = 0.3  # sample value
-    event_data.event.memory_usage = 0.1  # sample value
-    event_data.event.detail["sql"] = sql
-    event_data.event.description = "Create necessary tables in PostgreSQL database"
-    event_data.actor.detail["location"] = [
-        "server/main.py - line 100",
-        "server/database/managers.py - line 23",
-    ]
-    event_data.resource.detail["database"] = settings.POSTGRES_DB
-    event_data.resource.detail["tables"] = ["accounts"]
-
+    event_data = db_initialization_event(execution_time=execution_time, sql=sql)
     startup_events.append(event_data)
     print("Relational database and tables created!")
 
